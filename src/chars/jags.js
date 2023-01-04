77fcc0c50ae1e9796af166cebf3a7eaca2ce3637
Lauren Baca <laurenbaca1895@gmail.com>
2023-01-04 13:17:28 -0500

    added movements to both chars

--- src/chars/jags.js
+++ src/chars/jags.js
@@ -20,14 +20,45 @@ export class Jags extends Character {
     this.cursors.keyobj_w = this.scene.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.W
     );
-    this.cursors.keyobj_s = this.scene.input.keyboard.addKey(
-      Phaser.Input.Keyboard.KeyCodes.S
-    );
     this.cursors.keyobj_a = this.scene.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.A
+    );
+    this.cursors.keyobj_s = this.scene.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.S
     );
     this.cursors.keyobj_d = this.scene.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.D
     );
+  }
+
+  update() {
+    // MOVEMENT
+    // Move Left
+    if (this.cursors.keyobj_a.isDown) {
+      this.body.setVelocityX(-500);
+      this.flipX = true;
+
+      // Move Right
+    } else if (this.cursors.keyobj_d.isDown) {
+      this.body.setVelocityX(500);
+      this.flipX = false;
+
+      // Idle
+    } else {
+      this.body.setVelocityX(0);
+    }
+
+    // Grounded Jump
+    if (
+      Phaser.Input.Keyboard.JustDown(this.cursors.keyobj_w) &&
+      this.body.blocked.down
+    ) {
+      this.body.setVelocityY(-450);
+    }
+
+    // Fast-falling
+    if (this.cursors.keyobj_s.isDown && this.body.blocked.down) {
+      this.body.setVelocityY(400);
+    }
   }
 }
--- src/chars/lorn.js
+++ src/chars/lorn.js
@@ -4,6 +4,10 @@ export class Lorn extends Character {
   constructor(scene, x, y) {
     super(scene, x, y, 'lorn');
 
+    // Making the homie
+    this.scene.add.existing(this);
+    this.scene.physics.add.existing(this);
+
     this.init();
   }
 
@@ -19,14 +23,42 @@ export class Lorn extends Character {
     this.cursors.keyobj_up = this.scene.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.UP
     );
-    this.cursors.keyobj_down = this.scene.input.keyboard.addKey(
-      Phaser.Input.Keyboard.KeyCodes.DOWN
-    );
     this.cursors.keyobj_left = this.scene.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.LEFT
+    );
+    this.cursors.keyobj_down = this.scene.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.DOWN
     );
     this.cursors.keyobj_right = this.scene.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.RIGHT
     );
+  }
+
+  update() {
+    // MOVEMENT
+    // Move Left
+    if (this.cursors.keyobj_left.isDown) {
+      this.body.setVelocityX(-500);
+      this.flipX = true;
+
+      // Move Right
+    } else if (this.cursors.keyobj_right.isDown) {
+      this.body.setVelocityX(500);
+      this.flipX = false;
+
+      // Idle
+    } else {
+      this.body.setVelocityX(0);
+    }
+
+    // Grounded Jump
+    if (this.cursors.keyobj_up.isDown && this.body.blocked.down) {
+      this.body.setVelocityY(-450);
+    }
+
+    // Fast-falling
+    if (this.cursors.keyobj_s.isDown && this.body.blocked.down) {
+      this.body.setVelocityY(400);
+    }
   }
 }
