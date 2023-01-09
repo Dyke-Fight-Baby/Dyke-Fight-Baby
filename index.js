742f8d7da67fadd3875acf0f3c1785bb04868f11
Jackie Manginelli <jmanginelli11@gmail.com>
2023-01-09 11:40:17 -0500

    fixed up backend for sockets

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 'use strict';
 
-const app = require('./server/index');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+
+// Create instance of an express app
+const app = express();
+
+//body parsing middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // parse application/json
+app.use(cookieParser());
+
+// // require passport auth
+// require('./auth/auth');
+
+app.use(express.static(__dirname + '/public'));
+
+// sends index.html
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// error handling endware
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).json({ error: err.message });
+});
+
 const { db } = require('./server/db/index');
 const PORT = process.env.PORT || 3000;
 
